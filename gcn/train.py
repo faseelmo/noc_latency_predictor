deb5e07e587238eeb4e9011c394688b6400963c0
faseel <faseelm98@gmail.com>
2024-01-25 14:02:05 +0100

    training utils

--- gcn/train.py
+++ gcn/train.py
@@ -5,8 +5,9 @@ import torch.optim as optim
 from model import LatNet
 from dataset import load_data
 
-from tqdm import tqdm
+import time
 import pickle
+from tqdm import tqdm
 import matplotlib.pyplot as plt
 
 """Training Information """
@@ -21,11 +22,16 @@ DATA_DIR = '../training_data/data/task_7'
 INPUT_FEATURES = 4                                             #Node Level Features
 NUM_NODES = 9
 
+"""Load Model"""
+LOAD_MODEL = False
+MODEL_PATH = "results/"
+
 torch.manual_seed(1)
 
 def train_fn(train_loader, model, optimizer, loss_fn): 
     loop = tqdm(train_loader, leave=True)
 
+    mean_loss = []
     for batch_idx, data in enumerate(loop):
         output = model(data.to(DEVICE)).to(DEVICE)
         loss = loss_fn(output.view(-1), data.y)
@@ -35,6 +41,10 @@ def train_fn(train_loader, model, optimizer, loss_fn):
         optimizer.step()
 
         loop.set_postfix(loss=loss.item())
+        mean_loss.append(loss.item())
+
+    train_loss = sum(mean_loss)/len(mean_loss) 
+    return train_loss
 
 
 def validation_fn(test_loader, model, loss_fn, epoch):
@@ -48,23 +58,34 @@ def validation_fn(test_loader, model, loss_fn, epoch):
     print(f"[{epoch}] Validation MAE is {validation_set_loss}")
     return validation_set_loss
 
-def plot_and_save_loss(epoch_loss_list):
-    epochs, losses = zip(*epoch_loss_list)
-
-    plt.plot(epochs, losses, label='Loss')
-    plt.title('Loss Plot')
+def plot_and_save_loss(train_loss, valid_loss):
+    epochs = range(1, len(train_loss) + 1)
+    plt.plot(epochs, train_loss, label='Training Loss')
+    plt.plot(epochs, valid_loss, label='Validation Loss')
+    plt.title('Training and Validation Loss')
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.legend()
     plt.savefig('validation_plot.png')
+    plt.clf()
 
-    with open('validation_loss.pkl', 'wb') as file:
-        pickle.dump(epoch_loss_list, file)
+    loss_dict = {
+        'train_loss' : train_loss,
+        'valid_loss' : valid_loss,
+    }
+    with open('loss_dict.pkl', 'wb') as file:
+        pickle.dump(loss_dict, file)
 
 def main():
+    start_time = time.time()
     train_loader, test_loader = load_data(DATA_DIR, BATCH_SIZE)
 
     model = LatNet(INPUT_FEATURES, NUM_NODES).to(DEVICE)
+
+    if LOAD_MODEL:
+        model_state_dict = torch.load(MODEL_PATH)
+        model.load_state_dict(model_state_dict)
+        print(f"\nPre-Trained Wieghts Loaded\n")
 
     optimizer = optim.Adam(
         model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY
@@ -72,14 +93,22 @@ def main():
 
     loss_fn = nn.L1Loss()
 
-    validation_loss = []
+    valid_loss_list = []
+    train_loss_list = []
     for epoch in range(EPOCHS): 
-        train_fn(train_loader, model, optimizer, loss_fn)
-        loss = validation_fn(test_loader, model, loss_fn, epoch)
-        validation_loss.append((epoch, loss))
-        plot_and_save_loss(validation_loss)
+        train_loss = train_fn(train_loader, model, optimizer, loss_fn)
+        valid_loss = validation_fn(test_loader, model, loss_fn, epoch)
+
+        train_loss_list.append(train_loss)
+        valid_loss_list.append(valid_loss)
+
+        plot_and_save_loss(train_loss_list, valid_loss_list)
 
     torch.save(model.state_dict(), 'LatNet.pth')
+    end_time = time.time()
+    
+    time_elapsed = (end_time - start_time) / 60
+    print(f"\nTraining Time: {time_elapsed} minutes\n")
         
 if __name__ == "__main__":
     main()
